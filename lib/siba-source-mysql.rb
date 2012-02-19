be7bf81b1fa913c40da5470f21abb4bcae39c136
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2012-02-19 11:02:18 +0400

    Initial commit

---
+++ lib/siba-source-mysql.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+require "siba-source-mysql/version"
+require "siba-source-mysql/init"
+
+module Siba
+  module Source
+    module Mysql
+    end
+  end
+end
---
+++ lib/siba-source-mysql/version.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+module Siba
+  module Source
+    module Mysql
+      VERSION = "0.0.1"
+    end
+  end
+end
